85d3e8dc6bd00a73cb6cee4e09c347ebd357d63f
Cedric St-Jean <cedric.stjean@gmail.com>
2017-01-24 21:59:07 -0500

    Basic nbtranslate works

--- README.md
+++ README.md
@@ -5,3 +5,26 @@
 [![Coverage Status](https://coveralls.io/repos/cstjean/NBTesting.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/cstjean/NBTesting.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/cstjean/NBTesting.jl/coverage.svg?branch=master)](http://codecov.io/github/cstjean/NBTesting.jl?branch=master)
+
+`nbtest` produces a git-friendly `.jl` file with the tests before running them. It can
+be produced without running the tests by calling `NBTesting.nbtranslate(filename)`.
+
+The code in a cell that comes after `# NBSKIP` is ignored.
+
+```
+@test 5+5 == 10
+# NBSKIP
+@test 10*10 == This doesn't even have to be valid Julia code; it's not saved
+```
+
+
+
+
+TODO
+
+- export `is_testing()` which returns true iff we're running a test (use a dlet over the
+whole module - we want `include` to work)
+- verbose=1...5 corresponding to ##### Titles
+- @testset. Beware that this may create new scopes
+- Print the testing module name
+- Create a dummy module for Plots, that exports the same variables, but every function is a no-op
--- src/NBTesting.jl
+++ src/NBTesting.jl
@@ -1,5 +1,52 @@
-module NBTesting
+# Author: Cedric St-Jean
+# with some code taken from NBInclude.jl, by Steven G. Johnson
+# __precompile__()
 
-# package code goes here
+module NBTesting
+using JSON
+
+is_skip(line) = startswith(line, "#NBSKIP") || startswith(line, "# NBSKIP")
+
+function nbtranslate(path::AbstractString;
+                     outfile_name = "NBTest_" * splitext(path)[1] * ".jl")
+    _, ext = splitext(path)
+    @assert ext == ".ipynb" "nbtranslate only accepts notebook files (.ipynb)"
+
+    # sleep a bit to process file requests from other nodes
+    nprocs()>1 && sleep(0.005)
+    nb = open(JSON.parse, path, "r")
+
+    # check for an acceptable notebook:
+    nb["nbformat"] == 4 || error("unrecognized notebook format ", nb["nbformat"])
+    lang = lowercase(nb["metadata"]["language_info"]["name"])
+    lang == "julia" || error("notebook is for unregognized language $lang")
+
+    shell_or_help = r"^\s*[;?]" # pattern for shell command or help
+    
+    open(outfile_name, "w") do outfile
+        for (counter, cell) in enumerate(nb["cells"])
+            if cell["cell_type"] == "code" && !isempty(cell["source"])
+                s = join(cell["source"])
+                isempty(strip(s)) && continue # Jupyter doesn't number empty cells
+                ismatch(shell_or_help, s) && continue
+                cellnum = string(counter)
+                
+                lines = split(s, "\n")
+                if !is_skip(lines[1])
+                    write(outfile, string("# Cell #", cellnum, "\n"))
+                end
+                for line in lines
+                    if is_skip(line) break end
+                    write(outfile, line)
+                    write(outfile, "\n")
+                end
+                write(outfile, "\n")
+            end
+        end
+    end
+    return outfile_name
+end
+
+
 
 end # module
